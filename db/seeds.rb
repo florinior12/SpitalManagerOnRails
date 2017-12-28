79d3fd75f71c00b00477dce31cd482cb79968c0f
Florin Iordache <florin.gal12@gmail.com>
2017-12-28 23:44:33 +0200

    seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times do 
-  nume = Faker::Name.first_name
-  prenume = Faker::Name.last_name
-  varsta = (18..75).to_a.sample
-  adresa = Faker::Address.street_address
-  Pacient.create!(nume: nume, prenume: prenume, varsta: varsta, adresa: adresa)
-end
+# 50.times do 
+#   nume = Faker::Name.first_name
+#   prenume = Faker::Name.last_name
+#   varsta = (18..75).to_a.sample
+#   adresa = Faker::Address.street_address
+#   Pacient.create!(nume: nume, prenume: prenume, varsta: varsta, adresa: adresa)
+# end
 
-50.times do 
-  nume = Faker::Name.first_name
-  prenume = Faker::Name.last_name
-  sectie = Medic.sectii.sample
-  Medic.create!(nume: nume, prenume: prenume, sectie: sectie)
-end
+# 50.times do 
+#   nume = Faker::Name.first_name
+#   prenume = Faker::Name.last_name
+#   sectie = Medic.sectii.sample
+#   Medic.create!(nume: nume, prenume: prenume, sectie: sectie)
+# end
 
 book = Spreadsheet.open 'db/999cod.xls'
 sheet = book.worksheet 0
-50.times do 
+100.times do 
     id_medic = Medic.all.sample.id
     id_pacient = Pacient.all.sample.id
     diagnostic = sheet.cell((1..999).to_a.sample,1)
